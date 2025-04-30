5b2900386021bd51f936256e046dfd82ac0f6af0
Vishal <vishalantiya1111@gmail.com>
2025-04-30 18:48:47 +0530

    microservice update

--- src/main/java/com/turbo/E_commerce/platform/ECommercePlatformApplication.java
+++ src/main/java/com/turbo/E_commerce/platform/ECommercePlatformApplication.java
@@ -2,8 +2,10 @@ package com.turbo.E_commerce.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ECommercePlatformApplication {
 
 	public static void main(String[] args) {
